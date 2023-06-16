0b4d1d3c7b649756cebb18b20e6413242fffc10a
SinthiyaBalan <sinthiyamct@gmail.com>
2023-06-16 10:04:06 +0200

    Installed daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
       <p className='m-12'>Hi from React</p>
     </div>
   );
